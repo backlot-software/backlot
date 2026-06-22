b647dbfa41ae6732beb07aeb31497d972363867e
Jeroen Wijdeven <jwijdeven@chuhukon.nl>
2026-06-22 09:03:18 +0200

    obsolete marker

--- Backlot.Core/Abstraction/Roles/RolePlayExtensions.cs
+++ Backlot.Core/Abstraction/Roles/RolePlayExtensions.cs
@@ -10,6 +10,7 @@ namespace Backlot.Core.Abstraction.Roles;
 /// </summary>
 public static class RolePlayExtensions
 {
+        [Obsolete("Use ScenarioReference.Play instead. This will be removed in 1.0.0")]
         public static async Task<TResult> PlayAsync<TRole, TResult>(this TRole role, Func<TRole, TResult> func, Action<FuncScenario<TRole, TResult>> watch = null)
             where TRole : IRole
         {
@@ -20,7 +21,7 @@ public static class RolePlayExtensions
             return scene.ResultValue;
         }
         
-        [Obsolete("Use ScenarioReference.Play instead. This will be removed in 3.1.x")]
+        [Obsolete("Use ScenarioReference.Play instead. This will be removed in 1.0.0")]
         public static async Task<TResult> PlayAsync<TResult>(this IRole role, ScenarioReference scenario, Action<IScenario> watch = null)
         {
             var result = await PlayAsync(role, scenario, watch);
@@ -31,7 +32,7 @@ public static class RolePlayExtensions
                 $"Scenario '{scenario.Name}' for role '{role.GetType().Name}' does not return expected '{typeof(TResult).Name}'");
         }
         
-        [Obsolete("Use ScenarioReference.Play instead. This will be removed in 3.1.x")]
+        [Obsolete("Use ScenarioReference.Play instead. This will be removed in 1.0.0")]
         public static async Task<object> PlayAsync(this IRole role, ScenarioReference scenario, Action<IScenario> watch = null)
         {
             var scene = ScenarioBuilder.Construct(scenario, role);
@@ -41,7 +42,7 @@ public static class RolePlayExtensions
             return scene.ResultValue;
         }
         
-        [Obsolete("Use ScenarioReference.Play instead. This will be removed in 3.1.x")]
+        [Obsolete("Use ScenarioReference.Play instead. This will be removed in 1.0.0")]
         public static async Task<object> PlayAsync(this IEnumerable<IRole> roles, ScenarioReference scenario, Action<IScenario> watch = null)
         {
             var scene = ScenarioBuilder.Construct(scenario, roles);
@@ -51,14 +52,14 @@ public static class RolePlayExtensions
             return scene.ResultValue;
         }
         
-        [Obsolete("Use Scenario.Play instead. This will be removed in 3.1.x")]
+        [Obsolete("Use Scenario.Play instead. This will be removed in 1.0.0")]
         public static async Task<TResult> PlayAsync<TScenario,TResult>(this IRole role, Action<TScenario> watch = null, string named=null)
             where TScenario : IScenario<IRole, TResult>, IScenario
         {
             return (TResult) await role.PlayAsync(watch, named);
         }
         
-        [Obsolete("Use Scenario.Play instead. This will be removed in 3.1.x")]
+        [Obsolete("Use Scenario.Play instead. This will be removed in 1.0.0")]
         public static async Task<object> PlayAsync<TScenario>(this IRole role, Action<TScenario> watch = null, string named=null)
             where TScenario : IScenario
         {
@@ -69,7 +70,7 @@ public static class RolePlayExtensions
             return scene.ResultValue;
         }
         
-        [Obsolete("Use Scenario.Play instead. This will be removed in 3.1.x")]
+        [Obsolete("Use Scenario.Play instead. This will be removed in 1.0.0")]
         public static async Task<(bool IsSuccess, TResult Result)> PlayWhenValidAsync<TResult>(this IRole role, ScenarioReference scenario, Action<IScenario> watch = null)
         {
             var paw = await PlayWhenValidAsync(role, scenario, watch);
@@ -82,7 +83,7 @@ public static class RolePlayExtensions
             return (paw.IsSuccess, default);
         }
         
-        [Obsolete("Use Scenario.Play instead. This will be removed in 3.1.x")]
+        [Obsolete("Use Scenario.Play instead. This will be removed in 1.0.0")]
         public static async Task<(bool IsSuccess, object Result)> PlayWhenValidAsync(this IRole role, ScenarioReference scenario, Action<IScenario> watch = null)
         {
             var scene = ScenarioBuilder.Construct(scenario, role);
@@ -98,7 +99,7 @@ public static class RolePlayExtensions
             return (false, null);
         }
         
-        [Obsolete("Use Scenario.Play instead. This will be removed in 3.1.x")]
+        [Obsolete("Use Scenario.Play instead. This will be removed in 1.0.0")]
         public static async Task<object> PlayAsync<TScenario>(this IEnumerable<IRole> roles, Action<IScenario> watch = null, string named=null)
             where TScenario: IScenario
         {
