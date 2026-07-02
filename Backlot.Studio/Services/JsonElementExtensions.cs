269a68ef775a246c1834c6a4c9755705acde6dd7
Jeroen Wijdeven <jwijdeven@chuhukon.nl>
2026-07-02 16:50:04 +0200

    comment unwrap

--- Backlot.Studio/Services/JsonElementExtensions.cs
+++ Backlot.Studio/Services/JsonElementExtensions.cs
@@ -4,6 +4,15 @@ namespace Backlot.Studio.Services;
 
 public static class JsonElementExtensions
 {
+    /// <summary>
+    /// Attempts to extract a nested JSON object by its property name.
+    /// </summary>
+    /// <param name="body">The source <see cref="JsonElement"/>.</param>
+    /// <param name="propertyName">The name of the property to unwrap.</param>
+    /// <returns>
+    /// The nested <see cref="JsonElement"/> if the property is found and is an object; 
+    /// otherwise, the original <paramref name="body"/>.
+    /// </returns>
     public static JsonElement Unwrap(this JsonElement body, string propertyName)
     {
         if (body.ValueKind != JsonValueKind.Object)
